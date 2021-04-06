1fa4a4c849048f7b8c2423f3c47449c0a5e757ad
Maksym Lysenko <mlysenko0601@gmail.com>
2021-04-06 21:36:31 +0200

    languageContextStore was added

---
+++ src/components/Button.js
@@ -0,0 +1,31 @@
+import React from "react";
+import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
+
+class Button extends React.Component {
+  //   static contextType = LanguageContext; //contextType - have to be named only like that
+
+  renderSubmit = (language) => {
+    return language === "english" ? "Submit" : "Підтвердити";
+  };
+
+  renderButton(color) {
+    return (
+      <button className={`ui button ${color}`}>
+        <LanguageContext.Consumer>
+          {({ language }) => this.renderSubmit(language)}
+        </LanguageContext.Consumer>
+      </button>
+    );
+  }
+
+  render() {
+    return (
+      <ColorContext.Consumer>
+        {(color) => this.renderButton(color)}
+      </ColorContext.Consumer>
+    );
+  }
+}
+
+export default Button;
